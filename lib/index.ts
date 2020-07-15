423dc174538e67ec59b23996a15c97769fed813f
Sean Donnelly <sdonnelly3000@gmail.com>
2020-07-15 16:29:54 -0700

    :recycle: Export Additional HTTP Helpers from lib

--- lib/index.ts
+++ lib/index.ts
@@ -13,6 +13,9 @@ import { STARTING, READY } from './api/messages';
 
 // http helpers
 export { Route } from './api/http/route';
+export { Params } from './api/http/params';
+export { Authorize } from './api/http/authorize';
+export { Query } from './api/http/query';
 export { StatusCode } from './api/http/status';
 
 // core config
@@ -23,9 +26,6 @@ export default async (callback?: () => void) => {
 
   // data layer
   await connect();
-
-  // hydrate component layer
-  // Injector.hydrate();
 
   start(() => {
     READY();
